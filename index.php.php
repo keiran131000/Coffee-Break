7263ec4a4ba3f74468479315629c40d6088507e5
keiran131000 <33021776+keiran131000@users.noreply.github.com>
2017-10-24 12:46:02 +0100

    Unauth set up
    and meta refresh

--- index.php.php
+++ index.php.php
@@ -1,4 +1,4 @@
-<!--<meta http-equiv="refresh" content="20"> -->
+<meta http-equiv="refresh" content="2"> 
 
 <link href="https://fonts.googleapis.com/css?family=Fascinate+Inline" rel="stylesheet">
 
@@ -14,7 +14,7 @@
         font-family: 'Fascinate Inline', cursive;
         color:white;
         font-style: oblique;
-        font-size: 50px;
+        font-size: 30px;
         margin-left: 10px;
         background: blue;
         padding-top: 50px;
@@ -22,7 +22,9 @@
         padding-bottom: 30px;
         padding-left: 30px;
         padding-top: 30px;
-        margin-bottom: 10px
+        margin-bottom: 10px;
+        opacity: 0.7;
+        text-align: center;
 
 
 
@@ -83,7 +85,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT COUNT(*) AS 'bob' FROM `ricochet` WHERE `ExpanderId` = 1 AND `id` > 3780000 AND `DevNo` = 17 OR `DevNo` = 12 OR `DevNo` = 11 OR `DevNo` = 21 ";
+$sql = "SELECT COUNT(*) AS `bob` FROM `ricochet` WHERE `ExpanderId` = 1 AND `id` > 3925460 AND (`DevNo` = 8 OR `DevNo` = 12 OR `DevNo` = 11 OR `DevNo` = 21) ";
 
 $result = $conn->query($sql);
 
@@ -97,7 +99,7 @@ while($row = $result->fetch_assoc()){
 
 
 
-$sql = "SELECT COUNT(*) as `dave` FROM `ricochet` WHERE `DevNo`= 11 AND `ExpanderId` = 1 AND `id` > 3780000 ";
+$sql = "SELECT COUNT(*) as `dave` FROM `ricochet` WHERE `DevNo`= 25 AND `ExpanderId` = 1 AND `id` > 3925460 ";
 
 $result = $conn->query($sql);
 
@@ -105,12 +107,12 @@ while($row = $result->fetch_assoc()){
     $dave = $row['dave'];
 }
 
-$total = ($bob / $dave) -200 
+$total = ($dave / $bob ) * 100
 ?>
 
 
 <?php    
-    $sql = "SELECT COUNT(*) as `dave2` FROM `ricochet` WHERE `DevNo`= 27 AND `ExpanderId` = 1 AND `id` > 3780000 ";
+    $sql = "SELECT COUNT(*) as `dave2` FROM `ricochet` WHERE `DevNo`= 11 AND `ExpanderId` = 1 AND `id` > 3925460 ";
 
 $result = $conn->query($sql);
 
@@ -118,11 +120,11 @@ while($row = $result->fetch_assoc()){
     $dave2 = $row['dave2'];
 }
 
-$total2 = ($bob / $dave2) -200   
+$total2 = ($dave2 / $bob) * 100 
 ?>
 
 <?php    
-    $sql = "SELECT COUNT(*) as `dave3` FROM `ricochet` WHERE `DevNo`= 25 AND `ExpanderId` = 1 AND `id` > 3780000 ";
+    $sql = "SELECT COUNT(*) as `dave3` FROM `ricochet` WHERE `DevNo`= 27 AND `ExpanderId` = 1 AND `id` > 3925460 ";
 
 $result = $conn->query($sql);
 
@@ -130,11 +132,11 @@ while($row = $result->fetch_assoc()){
     $dave3 = $row['dave3'];
 }
 
-$total3 = ($bob / $dave3) -200 
+$total3 = ($dave3 / $bob)  * 100
 ?>
 
 <?php    
-    $sql = "SELECT COUNT(*) as `dave4` FROM `ricochet` WHERE `DevNo`= 8 AND `ExpanderId` = 1 AND `id` > 3780000  ";
+    $sql = "SELECT COUNT(*) as `dave4` FROM `ricochet` WHERE `DevNo`= 8 AND `ExpanderId` = 1 AND `id` > 3925460  ";
 
 $result = $conn->query($sql);
 
@@ -142,15 +144,32 @@ while($row = $result->fetch_assoc()){
     $dave4 = $row['dave4'];
 }
 
-$total4 = ($bob / $dave4) -200    
+$total4 = ($dave4 / $bob) * 100    
 ?>
 
+<?php    
+    $sql = "SELECT COUNT(*) as `brian` FROM `ricochet` WHERE `DevNo` = 18 AND `id` > 3925460 AND `Active` = 1 AND `ExpanderId` = 1 ";
+
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()){
+    $brian = $row['brian'];
+}
+
+
+?>
 
 <html>
 
 
     <head>
-
+<h1>
+    Unauthorised Access 
+    
+    <?php echo $brian; ?> times
+        
+        
+</h1>
         <img class = "title" src = "/Pac/Title.png">
 
     </head>
@@ -159,7 +178,10 @@ $total4 = ($bob / $dave4) -200
     <body style="background:black">
         <!--Set Bars-->
         <div class = "bar" id = "Bar 1" >
-            <h1>Marketing</h1>
+
+
+
+            <h1 id = "Marketing"> </h1>
             <img src = "/Ghosts/blue.gif" style="margin-left:80%; margin-right:0px;position:absolute; width:180px;height:180px;" id="ghost1">
 
             <img class = "Character" id = "1" style="Margin-Left:<?php echo $total; ?>%" src = "/Pac/giphy-pacman.gif">
@@ -169,21 +191,21 @@ $total4 = ($bob / $dave4) -200
 
 
         <div class = "bar" id = "Bar 2">
-            <h1>Tech Support</h1>
+            <h1 id = "Tech"> </h1>
             <img src = "/Ghosts/red.gif" style="margin-left:80%; margin-right:0px;position:absolute; width:180px;height:180px;" id="ghost2">
             <img class = "Character" id = "2" style="Margin-Left:<?php echo $total2; ?>%" src = "/Pac/giphy-pacman.gif">
 
         </div>
 
         <div class = "bar" id = "Bar 3">
-            <h1>Special Projects</h1>
+            <h1 id = "Special"> </h1>
             <img src = "/Ghosts/orange.gif" style="margin-left:80%; margin-right:0px;position:absolute; width:180px;height:180px;" id="ghost3">
             <img class = "Character" id = "3" style="Margin-Left:<?php echo $total3; ?>% ; border-left(solid)" src = "/Pac/giphy-pacman.gif">
 
         </div>
 
         <div class = "bar" id = "Bar 4">
-            <h1>Finance</h1>
+            <h1 id = "Finance"> </h1>
             <img src = "/Ghosts/pink.gif" style="margin-left:80%; margin-right:0px;position:absolute; width:180px;height:180px;" id="ghost4">
             <img class = "Character" id = "4" style="Margin-Left:<?php echo $total4; ?>%"  src = "/Pac/giphy-pacman.gif">
 
@@ -233,7 +255,7 @@ $total4 = ($bob / $dave4) -200
 
     <!--Translate-->
     <script>
-        if (parseInt(<?php echo $total; ?>) > 24 ){
+        if (parseInt(<?php echo $total; ?>) > 10 ){
             document.getElementById("Bar 1").style.backgroundImage = "url('/Bars/bar2.png')";
         }
         if (parseInt(<?php echo $total; ?>) > 25 ){
@@ -262,7 +284,7 @@ $total4 = ($bob / $dave4) -200
 
 
 
-        if (parseInt(<?php echo $total2; ?>) > 24 ){
+        if (parseInt(<?php echo $total2; ?>) > 10 ){
             document.getElementById("Bar 2").style.backgroundImage = "url('/Bars/bar2.png')";
         }
         if (parseInt(<?php echo $total2; ?>) > 25 ){
@@ -294,7 +316,7 @@ $total4 = ($bob / $dave4) -200
 
 
 
-        if (parseInt(<?php echo $total3; ?>) > 24 ){
+        if (parseInt(<?php echo $total3; ?>) > 10 ){
             document.getElementById("Bar 3").style.backgroundImage = "url('/Bars/bar2.png')";
         }
         if (parseInt(<?php echo $total3; ?>) > 25 ){
@@ -354,7 +376,10 @@ $total4 = ($bob / $dave4) -200
 
 
 
-
+        document.getElementById("Marketing").innerHTML = " Marketing </br> <?php echo substr($total,0,2); ?>%" ;
+        document.getElementById("Tech").innerHTML = " Tech Support </br> <?php echo substr($total2,0,2); ?>%" ;
+        document.getElementById("Special").innerHTML = " Special Projects </br> <?php echo substr($total3,0,2); ?>%" ;
+        document.getElementById("Finance").innerHTML = " Finance </br> <?php echo substr($total4,0,2); ?>%" ;
 
 
 
